6e86143eff6f2e37f5f57e0500386035f4dbfcf1
Sam Christensen <s.christensen@calendly.com>
2021-11-29 15:17:11 -0500

    create basic game

--- js/card_game.mjs
+++ js/card_game.mjs
@@ -1,11 +1,56 @@
 export class CardGame{
-    constructor(prompts, displayCallback) {
-        this.prompts = prompts;
-        this.displayCallback = displayCallback;
-        this.currentQuestion = 0;
-        this.displayCallback(prompts[this.currentQuestion])
+    constructor(prompts, displayCallback, advanceDelay) {
+        this.advanceDelay = advanceDelay || 0;
+        this.prompts = prompts || [];
+        this.displayCallback = displayCallback || (() => {});
+        this.currentPromptIndex = 0;
+        if (this.prompts.length > 0) {
+            this.displayCallback(this.currentPrompt())
+        }
     }
 
-    attemptAnswer(response) {
+    attemptResponse(response) {
+        if (response == this.currentCorrectResponse()) {
+            this.advancePrompt();
+            return true;
+        } else {
+            this.displayCallback("incorrect")
+            setTimeout(() => {
+                this.displayCallback(this.currentPrompt());
+            }, this.advanceDelay);
+            return false;
+        }
+    }
+
+    currentPrompt() {
+        return this.prompts[this.currentPromptIndex][0]
+    }
+
+    currentCorrectResponse() {
+        return this.prompts[this.currentPromptIndex][1]
+    }
+
+    advancePrompt() {
+        if (this.currentPromptIndex < this.prompts.length - 1) {
+            this.currentPromptIndex++;
+            this.displayCallback(this.currentPrompt())
+        } else {
+            this.displayCallback("Winner!")
+        }
+    }
+
+    static TimesTables(displayCallback, advanceDelay) {
+        let prompts = [...Array(100).keys()].map(number => [
+            `${Math.ceil(number / 10)} * ${number % 10}`,
+            Math.ceil(number / 10) * (number % 10)]
+        )
+
+        // Durstenfeld shuffle thanks to https://stackoverflow.com/a/12646864
+        for (let i = prompts.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [prompts[i], prompts[j]] = [prompts[j], prompts[i]];
+        }
+
+        return new CardGame(prompts, displayCallback, advanceDelay)
     }
 }
--- test/card_game.js
+++ test/card_game.js
@@ -1,15 +1,75 @@
 import { CardGame } from '../js/card_game.mjs'
 import { expect } from 'chai'
 
+function expectDisplayOf() {
+    let current = 0;
+    const expected = arguments;
+    return (prompt) => {
+        current++;
+        if (current === expected.length - 1) {
+            expected[current]()
+        }
+    }
+}
+
 describe('CardGame', function() {
     it('create an instance', function() {
         const cardGame = new CardGame();
         expect(cardGame).instanceOf(CardGame)
     });
+
     it('sets an initial question', function(done) {
         const cardGame = new CardGame([
-            ["1 + 1", 2]
-        ]);
-        done()
+                ["1 + 1", 2]
+            ],
+            expectDisplayOf("1 + 1", done)
+        )
+    })
+
+    it('updates to next question after response', function(done) {
+        const cardGame = new CardGame([
+                ["1 + 1", 2],
+                ["2 + 2", 4]
+            ],
+            expectDisplayOf("1 + 1", "2 + 2", done)
+        );
+
+        cardGame.attemptResponse(2)
+    })
+
+    it('does not advance to the next question on incorrect response', function(done) {
+        const cardGame = new CardGame([
+                ["1 + 1", 2],
+                ["2 + 2", 4]
+            ],
+            expectDisplayOf("1 + 1", "incorrect", "1 + 1", done)
+        );
+
+        cardGame.attemptResponse(0)
+    })
+
+    it('plays a whole game', function(done) {
+        const cardGame = new CardGame([
+                ["1 + 1", 2],
+                ["2 + 2", 4]
+            ],
+            expectDisplayOf("1 + 1", "2 + 2", "Winner!", done)
+        );
+
+        cardGame.attemptResponse(2)
+        cardGame.attemptResponse(4)
+    })
+
+    it('plays a whole game with incorrect attempts', function(done) {
+        const cardGame = new CardGame([
+                ["1 + 1", 2],
+                ["2 + 2", 4]
+            ],
+            expectDisplayOf("1 + 1", "incorrect", "1 + 1", "2 + 2", "Winner!", done)
+        );
+
+        cardGame.attemptResponse(2)
+        cardGame.attemptResponse(0)
+        cardGame.attemptResponse(4)
     })
 });
